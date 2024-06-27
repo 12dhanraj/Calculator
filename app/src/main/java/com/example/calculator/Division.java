ce94fbde7e29f538911256082d93a24a2ca1e622
12dhanraj <dhanrajlatish12@gmail.com>
2024-06-28 00:00:27 +0530

    program completed

---
+++ app/src/main/java/com/example/calculator/Division.java
@@ -0,0 +1,53 @@
+package com.example.calculator;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Division extends AppCompatActivity {
+    EditText e1,e2;
+    AppCompatButton b1,b2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_division);
+        e1=(EditText) findViewById(R.id.n1);
+        e2=(EditText) findViewById(R.id.n2);
+        b1=(AppCompatButton) findViewById(R.id.divbtn);
+        b2=(AppCompatButton) findViewById(R.id.bmbtn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int num1= Integer.parseInt(e1.getText().toString());
+                    int num2=Integer.parseInt((e2.getText().toString()));
+                    int num3=num1/num2;
+                    String s=String.valueOf(num3);
+                    Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();}
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
